e18c8b0ac0edb2aeb89cb06e7d52d1c7f24e0011
Ajit Singh Yadav <37411261+AjitGE@users.noreply.github.com>
2024-05-27 19:19:27 +0530

    Update jiotv.py

--- jiotv.py
+++ jiotv.py
@@ -2,7 +2,7 @@ import requests;
 import json;
 
 global allChannels
-url= "https://cricfy.nayeemparvez-jtv.workers.dev/bpk-tv/$ToReplace/Fallback/index.m3u8"
+url= "https://fifabangladesh.live/@RANAPKX73/stream.m3u8?id=$ToReplace&e-.m3u8"
 m3ustr = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mitthu786/tvepg/main/jiotv/epg.xml.gz"\n\n'
 
 with open("jiodata.json", "r") as savedChannelDetailInFile:
@@ -12,7 +12,7 @@ allchannels=savedChannels
 for channelList in allchannels:
         channelUrlId=channelList['logoUrl'].split('https://jiotv.catchup.cdn.jio.com/dare_images/images/')[1].replace('.png','')
         m3ustr += "#EXTINF:-1 "
-        m3ustr += "tvg-id="+ "\"" + str(channelList['channel_id']) + "\" " + "group-title=" + "\"" + channelList['channelCategoryId'] + "\" " "tvg-logo=\""+str(channelList['logoUrl']) + "\"," + channelList['channel_name'] + "\n"+ url.replace("$ToReplace",channelUrlId+'_MOB')+"\n\n"
+        m3ustr += "tvg-id="+ "\"" + str(channelList['channel_id']) + "\" " + "group-title=" + "\"" + channelList['channelCategoryId'] + "\" " "tvg-logo=\""+str(channelList['logoUrl']) + "\"," + channelList['channel_name'] + "\n"+ url.replace("$ToReplace",channelList['channel_id'])+"\n\n"
 
 
 with open("allChannelPlaylist.m3u", "w") as allChannelPlaylistFile:
