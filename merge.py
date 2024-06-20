8c805ad0db46936d7f2fe853f4203f506ef1430f
AJITGE <ajitsinghyadav6@gmail.com>
2024-06-21 01:37:23 +0530

    updated

--- merge.py
+++ merge.py
@@ -4,26 +4,23 @@ import json;
 global allChannels
 with open("config.json", "r") as savedChannelDetailInFile:
     savedChannels = json.load(savedChannelDetailInFile)
-allchannelsnew=savedChannels
+allchannels=savedChannels
+
+result = dict()
+for channel in allchannels:
+    result[channel['channel_id']] = channel
 
 
 url = "https://tplayapi.code-crafters.app/321codecrafters/fetcher.json"
 x = requests.get(url)
 channel_list = x.json()['data']['channels']
 
-result = dict()
-for channel in channel_list:
-    result[channel['id']] = channel
 
-for channelnew in allchannelsnew:
-  if channelnew['channel_id'] in result:
-        channelnew['channel_license_url']=result[channelnew['channel_id']]['license_url']
-        channelnew['channel_url'] = result[channelnew['channel_id']]['manifest_url']
-  else :
-     print(channelnew['channel_id'])
+for channel in channel_list:
+  if channel['id'] in result:
+    channel['logo_url']=result[channel['id']]['channel_logo']
+    channel["genres"] = [result[channel['id']]['channel_genre']]
 
 with open("allChannels.json", "w") as channel_list_file:
-        json.dump(allchannelsnew, channel_list_file)
+        json.dump(channel_list, channel_list_file)
         channel_list_file.close()
-
-
